f814a105c1425411cd354e13182bd5c9f22edfd1
BiaTrivillin <biatrivillin.code@gmail.com>
2024-01-25 17:26:26 -0300

    created Game page style file

---
+++ src/components/layout/GameWaningTitle.js
@@ -0,0 +1,9 @@
+function GameWarning ({title}) {
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+export default GameWarning
--- src/components/pages/Game.js
+++ src/components/pages/Game.js
@@ -1,13 +1,48 @@
 import GameWarning from "../layout/GameWarning"
 import Header from '../layout/Header'
 import Footer from "../layout/Footer"
+import Button from "../layout/Button"
+import GameWarningTitle from "../layout/GameWaningTitle"
+import BordRockHand from '../../imgs/bord-rock-hand.png'
+import BordPaperHand from '../../imgs/bord-paper-hand.png'
+import BordScissorsHand from '../../imgs/bord-scissors-hand.png'
+import BordLizardHand from '../../imgs/bord-lizard-hand.png'
+import BordSpockHand from '../../imgs/bord-spock-hand.png'
+import RockHand from '../../imgs/rock-hand.png'
+import '../../styles/Game.css'
 
 function Game() {
     return (
         <div className="content">
             <Header/>
-            <h1>Game</h1>
-            <GameWarning title='WON' description='ipsum lorem dolor sit amat '/>
+            <section className="warning_title_container">
+                <GameWarningTitle title='BAZINGA!'/>
+            </section>
+            <section className="game_container">
+                <div className="game_content">
+                    <h1>YOU</h1>
+                    <h1>COMPUTER</h1>
+                    <div>
+                        <img src={RockHand} alt=""/>
+                    </div>
+                    <div>
+                        <img src={RockHand} alt=""/>
+                    </div>
+                </div>
+            </section>
+
+            <section className="selector_container">
+                <img src={BordRockHand} alt=""/>
+                <img src={BordPaperHand} alt=""/>
+                <img src={BordScissorsHand} alt=""/>
+                <img src={BordLizardHand} alt=""/>
+                <img src={BordSpockHand} alt=""/>
+            </section>
+
+            <section className="warning_container">
+                <GameWarning title='WON' description='ipsum lorem dolor sit amat '/>
+                <Button cta='PLAY AGAIN' id='play_again_btn'/>
+            </section>   
             <Footer/>
         </div>
     )
